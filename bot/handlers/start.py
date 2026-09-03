4c9002ca1ae24b4d6c07419c3f16f6ed0a6dfadd
mejavokh <tavhidovjavohir@gmail.com>
2026-09-03 22:06:56 +0500

    Добавлено получение услуг с обработкой ошибок API

--- bot/handlers/start.py
+++ bot/handlers/start.py
@@ -1,13 +1,34 @@
+import aiohttp
+import logging
 from aiogram import Router
 from aiogram.filters import CommandStart, Command
 from aiogram.types import Message
+from aiohttp import ClientConnectorError
 
 router = Router()
+URL = "http://127.0.0.1:8000"
+logger = logging.getLogger(__name__)
 
 @router.message(CommandStart())
 async def command_start(message: Message) -> None:
     await message.answer("Bot is working")
 
 @router.message(Command("services"))
-async def command_service(message: Message) -> None:
-    await message.answer("Here will be services")
+async def command_service(message: Message):
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(f"{URL}/services/") as response:
+                services = await response.json()
+                logger.info(f"\nкол-во услуг: {len(services)}\n"
+                            f"список услуг: {services}")
+
+        if services:
+            for service in services:
+                await message.answer(f"{service['name']} -> {service['price']}")
+        else:
+            await message.answer("Сервисы пока недоступны")
+    except ClientConnectorError:
+        logger.exception("Не удалось подключиться к сервису")
+        await message.answer("Что-то пошло не так, попробуйте позже")
+
+
